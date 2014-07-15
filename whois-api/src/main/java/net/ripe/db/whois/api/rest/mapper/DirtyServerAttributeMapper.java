370f7eeeba47fefd32697070315f38335c0c444f
Tom Harrison <tomh@apnic.net>
2014-07-15 14:25:49 +1000

    restore ReferencedTypeResolver use in DirtyServerAttributeMapper and FormattedServerAttributeMapper

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/DirtyServerAttributeMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/DirtyServerAttributeMapper.java
@@ -1,6 +1,6 @@
 package net.ripe.db.whois.api.rest.mapper;
 
-//import net.ripe.db.whois.api.rest.ReferencedTypeResolver;
+import net.ripe.db.whois.api.rest.ReferencedTypeResolver;
 import net.ripe.db.whois.api.rest.domain.Attribute;
 import net.ripe.db.whois.api.rest.domain.Link;
 import net.ripe.db.whois.common.utils.CIString;
@@ -16,11 +16,12 @@ import java.util.Set;
 
 @Component
 public class DirtyServerAttributeMapper implements AttributeMapper {
-//    private final ReferencedTypeResolver referencedTypeResolver;
+    private final ReferencedTypeResolver referencedTypeResolver;
     private final String baseUrl;
 
     @Autowired
-    public DirtyServerAttributeMapper(@Value("${api.rest.baseurl}") final String baseUrl) {
+    public DirtyServerAttributeMapper(final ReferencedTypeResolver referencedTypeResolver, @Value("${api.rest.baseurl}") final String baseUrl) {
+        this.referencedTypeResolver = referencedTypeResolver;
         this.baseUrl = baseUrl;
     }
 
@@ -36,8 +37,8 @@ public class DirtyServerAttributeMapper implements AttributeMapper {
         if (cleanValues.size() == 1) {
             // TODO: [AH] for each person or role reference returned, we make an sql lookup - baaad
             final CIString cleanValue = cleanValues.iterator().next();
-            final String referencedType = null; //(rpslAttribute.getType() != null) ? referencedTypeResolver.getReferencedType(rpslAttribute.getType(), cleanValue) : null;
-            final Link link = null; //(referencedType != null) ? createLink(source, referencedType, cleanValue.toString()) : null;
+            final String referencedType = (rpslAttribute.getType() != null) ? referencedTypeResolver.getReferencedType(rpslAttribute.getType(), cleanValue) : null;
+            final Link link = (referencedType != null) ? createLink(source, referencedType, cleanValue.toString()) : null;
             return Collections.singleton(new Attribute(rpslAttribute.getKey(), rpslAttribute.getFormattedValue(), null, referencedType, link));
         } else {
             return Collections.singleton(new Attribute(rpslAttribute.getKey(), rpslAttribute.getFormattedValue(), null, null, null));
